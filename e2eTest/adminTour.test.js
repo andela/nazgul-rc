aed21b907f330299bc080b1b28319de53a3dedf1
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2018-05-09 11:19:25 +0100

    feat(onboard-vendor): onboard vendor to the vendor dashboard
    - On board vendor
    - Write e2e test
    
    [Finishes #156958157]

---
+++ e2eTest/adminTour.test.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-expressions */
+const path = require("path");
+// accounts - dropdown
+const APP_BASE_PATH = "http://localhost:3000";
+
+module.exports = {
+  "User should be able to take tour anytime": browser => {
+    browser
+      .url(APP_BASE_PATH)
+      .waitForElementVisible("body", 5000)
+      .assert.urlEquals(`${APP_BASE_PATH}/`)
+      .pause(2000)
+      .assert.visible("body")
+      .assert.visible("span")
+      .assert.visible(".accounts-dropdown")
+      .click(".accounts-dropdown")
+      .setValue(".email-edit-input", "admin@localhost")
+      .setValue(".password-edit-input", "r3@cti0n")
+      .click(".rui.btn.btn-primary.solid.btn-block")
+      .pause(3000)
+      .click(".take-tour")
+      .assert.containsText(
+        ".introjs-tooltiptext > h3",
+        "Welcome to Reaction Commerce"
+      );
+    // .waitForElementVisible(".container-medium.index > h5", 1000);
+  }
+};
